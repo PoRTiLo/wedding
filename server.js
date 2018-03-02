81c25ca5ae824b972650f9c14fdd5c393073d603
portilo@seznam.cz <portilo@seznam.cz>
2018-03-02 22:43:28 +0100

    CHG revert server.js

--- server.js
+++ server.js
@@ -6,4 +6,12 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.listen(process.env.PORT || 8080);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+const port = process.env.PORT || 3000;
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, () => console.log('running'));
