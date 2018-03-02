8f88b6b7024850655e1efa4daaf32c2542c7b98c
portilo@seznam.cz <portilo@seznam.cz>
2018-03-02 22:34:08 +0100

    CHG heroku by video

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const http = require('http')
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.listen(process.env.PORT || 8080);
