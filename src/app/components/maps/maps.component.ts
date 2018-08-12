5413443ad95b6f5067fc8ea47e5b2b48e767368e
portilo@seznam.cz <portilo@seznam.cz>
2018-08-12 09:39:48 +0200

    CHG maps, foto

--- src/app/components/maps/maps.component.ts
+++ src/app/components/maps/maps.component.ts
@@ -6,12 +6,14 @@ type MapType = 'fiance' | 'church' | 'feast' | 'route';
 // position of church
 const CHURCH_LAT_LNG = new google.maps.LatLng(49.610413, 17.739728);
 const FEAST_LAT_LNG = new google.maps.LatLng(49.587395, 17.800759);
-const BRIDGE_LAT_LNG = new google.maps.LatLng(49.5596389, 17.7302739);
+const BRIDE_LAT_LNG = new google.maps.LatLng(49.5596389, 17.7302739);
 const GROOM_LAT_LNG = new google.maps.LatLng(49.5593917, 17.7368614);
 
 // icon
 const CHURCH_ICON = '\uf51d';
-const FEAST_ICON = '';
+const FEAST_ICON = '\uf2e7';
+const BRIDE_ICON = '\uf182';
+const GROOM_ICON = '\uf183';
 // description of marker
 const CHURCH_INFO_CONTENT: string = `<div id="content">
     <div id="siteNotice">
@@ -22,7 +24,12 @@ const CHURCH_INFO_CONTENT: string = `<div id="content">
     <p>Parkoviště je vedle kostela. </p>
     </div>
     </div>`;
-const FEAST_INFO_CONTENT: string = ``;
+// Místo hostiny.
+const FEAST_INFO_CONTENT: string = `Místo hostiny. Zde bude probíhat celé veselí :).`;
+// Bydliště a přípravna Verči.
+const BRIDE_INFO_CONTENT: string = `Bydliště a přípravna Verči.`;
+// Bydliště a přípravna Jardy.
+const GROOM_INFO_CONTENT: string = `Bydliště a přípravna Jardy.`;
 
 @Component({
   selector: 'rt-maps',
@@ -34,8 +41,10 @@ export class RtMapsComponent implements OnInit {
   @ViewChild('gmap') gMapElement: ElementRef;
   map: google.maps.Map;
   marker: google.maps.Marker;
+  marker2: google.maps.Marker;
   infoWindow: google.maps.InfoWindow;
-  mapType: MapType = 'church';
+  infoWindow2: google.maps.InfoWindow;
+  mapType: MapType = 'fiance';
   address: string;
 
   private directionsService = new google.maps.DirectionsService;
@@ -48,14 +57,15 @@ export class RtMapsComponent implements OnInit {
   }
 
   private initMaps() {
-    const latLang = this.mapType === 'church' ? CHURCH_LAT_LNG : FEAST_LAT_LNG;
+    const latLang = BRIDE_LAT_LNG;
+    const latLang2 = GROOM_LAT_LNG;
     // disable name of business (company names)
     const styledMapType: google.maps.MapTypeStyle[] = [{featureType: 'poi.business', stylers: [{visibility: 'off'}]}];
 
     const mapOptions = {
       // required fields
       center: new google.maps.LatLng(latLang.lat(), latLang.lng()),
-      zoom: 11,
+      zoom: 14,
       mapTypeId: google.maps.MapTypeId.ROADMAP,
       // optional fields
       streetViewControl: false,
@@ -68,7 +78,10 @@ export class RtMapsComponent implements OnInit {
 
     // create info popup
     this.infoWindow = new google.maps.InfoWindow({
-        content: CHURCH_INFO_CONTENT
+        content: BRIDE_INFO_CONTENT
+    });
+    this.infoWindow2 = new google.maps.InfoWindow({
+      content: GROOM_INFO_CONTENT
     });
 
     // create marker
@@ -77,15 +90,31 @@ export class RtMapsComponent implements OnInit {
       icon: '',
       label: {
         fontFamily: '"Font Awesome 5 Free"',
-        text: CHURCH_ICON,
+        text: BRIDE_ICON,
         color: 'black'
       },
       map: this.map,
         optimized: false
     });
+    // create marker
+    this.marker2 = new google.maps.Marker({
+      position: new google.maps.LatLng(latLang2.lat(), latLang2.lng()),
+      icon: '',
+      label: {
+        fontFamily: '"Font Awesome 5 Free"',
+        text: GROOM_ICON,
+        color: 'black'
+      },
+      map: this.map,
+      optimized: false
+    });
     // register info popup
     this.marker.addListener('click', () => {
         this.infoWindow.open(this.map, this.marker);
+    });
+    // register info popup
+    this.marker2.addListener('click', () => {
+      this.infoWindow2.open(this.map, this.marker2);
     });
 
     this.directionsDisplay.setMap(this.map);
@@ -99,17 +128,38 @@ export class RtMapsComponent implements OnInit {
         this.directionsDisplay.setMap(this.map);
         this.directionsDisplay.setDirections(this.directionResult);
         this.marker.setMap(null);
+        this.marker2.setMap(null);
         break;
       default:
-        const latLang = this.mapType === 'church' ? CHURCH_LAT_LNG : FEAST_LAT_LNG;
-        this.map.setCenter(new google.maps.LatLng(latLang.lat(), latLang.lng()));
-        this.marker.setPosition(new google.maps.LatLng(latLang.lat(), latLang.lng()));
-        this.marker.getLabel().text = this.mapType === 'church' ? CHURCH_ICON : FEAST_ICON;
-        this.infoWindow.setContent(this.mapType === 'church' ? CHURCH_INFO_CONTENT : FEAST_INFO_CONTENT);
-        this.map.setZoom(11);
+        if (this.mapType === 'fiance') {
+          // verca
+          const latLang = BRIDE_LAT_LNG;
+          this.map.setCenter(new google.maps.LatLng(latLang.lat(), latLang.lng()));
+          this.marker.setPosition(new google.maps.LatLng(latLang.lat(), latLang.lng()));
+          this.marker.getLabel().text = BRIDE_ICON;
+          this.infoWindow.setContent(BRIDE_INFO_CONTENT);
+          // ja
+          const latLang2 = GROOM_LAT_LNG;
+          this.marker2.setPosition(new google.maps.LatLng(latLang2.lat(), latLang2.lng()));
+          this.marker2.getLabel().text = GROOM_ICON;
+          this.infoWindow2.setContent(GROOM_INFO_CONTENT);
+          this.marker2.setMap(this.map);
+          // common
+          this.map.setZoom(14);
+        } else {
+          const latLang = this.mapType === 'church' ? CHURCH_LAT_LNG : FEAST_LAT_LNG;
+          this.map.setCenter(new google.maps.LatLng(latLang.lat(), latLang.lng()));
+          this.marker.setPosition(new google.maps.LatLng(latLang.lat(), latLang.lng()));
+          this.marker.getLabel().text = this.mapType === 'church' ? CHURCH_ICON : FEAST_ICON;
+          this.infoWindow.setContent(this.mapType === 'church' ? CHURCH_INFO_CONTENT : FEAST_INFO_CONTENT);
+          // clear me
+          this.marker2.setMap(null);
+          this.map.setZoom(11);
+        }
         this.directionsDisplay.setMap(null);
         this.marker.setMap(this.map);
         this.infoWindow.close();
+        this.infoWindow2.close();
         break;
     }
   }
